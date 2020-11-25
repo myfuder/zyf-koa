fcd24cd91746881e73364f99bda4cd44b74fcd32
myfyder <1310728600@qq.com>
2020-11-25 17:24:20 +0800

    'reset'

---
+++ build/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//
+// console.log("webpack.config")
+module.exports = {
+
+    entry: {
+        main:'app.js'
+    }
+}
